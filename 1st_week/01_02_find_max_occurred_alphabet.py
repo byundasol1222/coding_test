843c6a0ef5135b8821a14d797d99209320ba52fd
byundasol1222 <byundasol1222@gmail.com>
2025-11-25 14:47:34 +0900

    [251125] 최빈값 찾기

---
+++ 1st_week/01_02_find_max_occurred_alphabet.py
@@ -0,0 +1,27 @@
+# Q. 다음과 같은 문자열을 입력받았을 때, 어떤 알파벳이 가장 많이 포함되어 있는지 반환하시오.
+# (단 최빈값을 가진 알파벳이 여러개일 경우 알파벳 순서가 가장 앞에 위치한 알파벳을 출력하시오)
+# "hello my name is dingcodingco"
+
+
+def find_max_occurred_alphabet(string):
+    alphabet_occurrence_array = [0] * 26
+
+    for char in string:
+        if not char.isalpha():
+            continue
+        arr_index = ord(char) - ord('a')
+        alphabet_occurrence_array[arr_index] += 1
+
+    max_occurrence = alphabet_occurrence_array[0]
+    max_index = 0
+    for arr_index, occurrence in enumerate(alphabet_occurrence_array):
+        if occurrence > max_occurrence:
+            max_occurrence = occurrence
+            max_index = arr_index
+    return chr(max_index + ord('a'))
+
+
+result = find_max_occurred_alphabet
+print("정답 = i 현재 풀이 값 =", result("hello my name is dingcodingco"))
+print("정답 = e 현재 풀이 값 =", result("we love algorithm"))
+print("정답 = b 현재 풀이 값 =", result("best of best youtube"))
